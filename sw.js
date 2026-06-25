dc19d6caf5c0df3d4eeed9ba5a79b24e88984d2e
まじまじ <majimakenta@gmail.com>
2026-06-25 22:27:48 +0900

    fix: SW外部ドメインブロック修正

--- sw.js
+++ sw.js
@@ -2,5 +2,9 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 self.addEventListener('fetch', e => {
+  // 外部ドメインはService Workerを通さず直接fetch
+  if (!e.request.url.startsWith(self.location.origin)) {
+    return;
+  }
   e.respondWith(fetch(e.request));
 });
