95d97314a025d172afd58b2cadd6b7c9dbe2e9e3
tarachiri <nukadokonokai@gmail.com>
2026-06-29 20:36:33 +0900

    feat: ブログ記事3件追加・index整備・不要スクリプト削除

--- blog/posts/add010.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-content = open('/Users/pro2015/danshu-de-go/blog/index.html').read()
-new_entry = '{file:"posts/010.html",title:"断酒、例会、みんな行けるようになるといいね。GPS現在地表示・かもちゃん決定木チャット完成",date:"2026-06-25",tags:["GPS","チャット","決定木","かもちゃん","UX"]},'
-content = content.replace('const POSTS=[', 'const POSTS=[\n' + new_entry)
-open('/Users/pro2015/danshu-de-go/blog/index.html', 'w').write(content)
-print('done')
